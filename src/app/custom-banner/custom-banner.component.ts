ff6f481f5b13fb44263467ad07c72c4b77c46315
ari <ari83seeker@yahoo.com>
2023-02-23 17:05:08 +0100

    aa

--- src/app/custom-banner/custom-banner.component.ts
+++ src/app/custom-banner/custom-banner.component.ts
@@ -3,29 +3,27 @@ import { ProductService } from '@spartacus/core';
 import { interval } from 'rxjs';
 import { filter, map, switchMap } from 'rxjs/operators';
 
-const PRODUCT_ID = ['358639', '932577', '1641905', '553637']
+const PRODUCT_ID = ['358639', '932577', '1641905', '553637', '1992693'];
 
 @Component({
   selector: 'app-custom-banner',
   templateUrl: './custom-banner.component.html',
-  styleUrls: ['./custom-banner.component.scss']
+  styleUrls: ['./custom-banner.component.scss'],
 })
 export class CustomBannerComponent implements OnInit {
-productCode$ = interval(2000).pipe(
-  map(()=> this.randomProductId())
-)
+  productCode$ = interval(2000).pipe(map(() => this.randomProductId()));
 
-product$ = this.productCode$.pipe(
-  switchMap(code => this.productService.get(code)),
-  filter(x => !!x)
-)
-  constructor(private productService: ProductService) { }
+  product$ = this.productCode$.pipe(
+    switchMap((code) => this.productService.get(code)),
+    filter((x) => !!x)
+  );
 
-  randomProductId(): string{
-    const index = Math.floor(Math.random() * PRODUCT_ID.length)
-    return PRODUCT_ID[index]
-  }
-  ngOnInit(): void {
+  constructor(private productService: ProductService) {}
+
+  randomProductId(): string {
+    const index = Math.floor(Math.random() * PRODUCT_ID.length);
+    return PRODUCT_ID[index];
   }
 
+  ngOnInit(): void {}
 }
